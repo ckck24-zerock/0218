51502ed537a0e7cb4799131c75f9f8faa365ecd5
zerock <kangckck24@gmail.com>
2025-02-21 11:31:39 +0900

    closure 1

--- public/todo.js
+++ public/todo.js
@@ -5,7 +5,14 @@ export default function TodoSerivce() {
     let idx = 0
     let arr = [ ]
 
-    function add() {  }
+    function add(title) {
+
+        const obj = {tno: idx++, title:title}
+
+        arr.push(obj)
+
+        return [...arr]
+    }
 
     function remove() {  }
 
