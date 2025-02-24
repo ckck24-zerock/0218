77393383adf8045b6e05f3032c4a8ef087217c93
zerock <kangckck24@gmail.com>
2025-02-24 10:55:39 +0900

    closure 1

--- public/cart.js
+++ public/cart.js
@@ -4,8 +4,16 @@ export default function cart() {
 
     let items = [] //장바구니 상품들
 
-    function add (item) {
-        //items에 추가
+    //메뉴 객체를 파라미터로 받아서 처리
+    function add (menu) {
+
+        const {mno,mname,price} = menu
+
+        const cartItem = {mno:mno, mname:mname,price:price, qty: 1}
+
+        items.push(cartItem)
+
+        return [...items]
     }
 
     function getItems () {
